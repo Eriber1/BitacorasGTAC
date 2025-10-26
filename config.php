ad9f8736d862995fb5c4586dff361fbbf0413b4f
Eriber <111772796+Eriber1@users.noreply.github.com>
2025-10-26 00:37:20 -0600

    se cambia el nombre del archivo con el que se guarda la bitacora

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // Configuración de la base de datos
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', 'AdminBitacoras@2024');
+define('DB_PASS', '');
 define('DB_NAME', 'bitacoras_gtac');
 
 // Configuración de subida de archivos
